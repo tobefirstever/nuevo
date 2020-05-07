3a973877943bec0c58578edec03b761784cf57e8
tobefirstever <tobefirstever@users.noreply.github.com>
2020-05-07 13:05:38 -0500

    se
    se

---
+++ CodeGenerate/ClassGen.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace MyApp
+{
+	public class RandomClass
+	{
+					public string prop0 {get;set;}
+						public string prop1 {get;set;}
+						public string prop2 {get;set;}
+						public string prop3 {get;set;}
+						public string prop4 {get;set;}
+				}
+}
+
